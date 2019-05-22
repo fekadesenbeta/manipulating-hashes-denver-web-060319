6b64d03ecfc5cf21154a78e6baedabb7b250300c
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-22 01:00:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,8 @@ def first_challenge
     }
   }
   
-contacts.each do |person, date|
-  date.each do |attribute, value|
+contacts.each do |person, data|
+  data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
         if flavor == "strawberry"
